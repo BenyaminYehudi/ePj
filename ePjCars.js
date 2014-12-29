ff397b785137f58cc65c682635ff4578972b2935
BenyaminYehudi <benyamin.yehudi@gmail.com>
2014-12-29 20:42:34 +0200

    Update ePjCars.js

--- ePjCars.js
+++ ePjCars.js
@@ -9,8 +9,8 @@ function Car (licensePlate, transmissionType, company, year, carModel, basePrice
 
 $(function(){
 
-var picCount=0;  
-var picArray= ["https://www.alamo.com/alamoData/vehicle/bookingCountries/US/CARS/ECAR.doi.320.high.imageSmallSideProfileNodePath.png/1408023566067.png",
+var imgCount=0;  
+var imgArray= ["https://www.alamo.com/alamoData/vehicle/bookingCountries/US/CARS/ECAR.doi.320.high.imageSmallSideProfileNodePath.png/1408023566067.png",
 "https://www.alamo.com/alamoData/vehicle/bookingCountries/US/CARS/CCAR.doi.320.high.imageSmallSideProfileNodePath.png/1408023598583.png",
 "https://www.alamo.com/alamoData/vehicle/bookingCountries/US/CARS/CXAR.doi.320.high.imageSmallSideProfileNodePath.png/1408029275863.png",
 "https://www.alamo.com/alamoData/vehicle/bookingCountries/US/CARS/ICAR.doi.320.high.imageSmallSideProfileNodePath.png/1408023634360.png",
@@ -21,13 +21,13 @@ var picArray= ["https://www.alamo.com/alamoData/vehicle/bookingCountries/US/CARS
 "https://www.alamo.com/alamoData/vehicle/bookingCountries/US/CARS/FCDR.doi.320.high.imageSmallSideProfileNodePath.png/1408029356219.png",
 "https://www.alamo.com/alamoData/vehicle/bookingCountries/US/CARS/GXAR.doi.320.high.imageSmallSideProfileNodePath.png/1408029442186.png"];
     
-    nextPic();
+    nextImage();
     
- function nextPic()
-  {  picCount=(picCount+1<picArray.length)? picCount+1 : 0;
-     var build='<img border="0" src="'+picArray[picCount]+
+ function nextImage()
+  {  imgCount=(imgCount+1<imgArray.length)? imgCount+1 : 0;
+     var newImage='<img border="0" src="'+imgArray[imgCount]+
      '" style="width:300px; height:200px;" />';
-     document.getElementById("changingCars").innerHTML=build;
-   setTimeout(function(){nextPic();},3000);
+     document.getElementById("changingCars").innerHTML=newImage;
+   setTimeout(function(){nextImage();},3000);
   }
 });
