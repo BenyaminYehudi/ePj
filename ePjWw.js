e4acf4ddd233f85dd415ddb0c835146674f2d2bc
BenyaminYehudi <benyamin.yehudi@gmail.com>
2014-12-31 13:26:40 +0200

    Update ePjWw.js

--- ePjWw.js
+++ ePjWw.js
@@ -1,1 +1,13 @@
-function rawr!
+var i= 0;
+
+function multiplesOfSeven() {
+    while(true){
+        i++
+        if(i%7==0){
+            postMessage(i);   
+        }
+         setTimeout("multiplesOfSeven()",2500);
+    }
+}
+
+multiplesOfSeven();
