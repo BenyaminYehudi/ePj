3a819f7db227f1743bcbe8af3124c67f9e5e5a37
BenyaminYehudi <benyamin.yehudi@gmail.com>
2015-01-03 23:58:44 +0200

    Update ePjRegister.js

--- ePjRegister.js
+++ ePjRegister.js
@@ -1,3 +1,5 @@
+$( document ).ready(function() {
+	
 function UserRegistration (userName, password, email, birthDate, homePhone, mobilePhone, licenseNumber) {
 	this.userName = userName;
 	this.password = password;
@@ -8,8 +10,6 @@ function UserRegistration (userName, password, email, birthDate, homePhone, mobi
 	this.licenseNumber = licenseNumber;
 };
 
-$( document ).ready(function() {
-
 
 var start = 1900;
 var end = new Date().getFullYear();
@@ -19,7 +19,18 @@ for(var year = start ; year <=end; year++){
 }
 document.getElementById("bYear").innerHTML = options;
 
-
+function register(){
+	var userName = $("#registerUsername").val();
+	var password = $("#registerPassword").val();
+	var email= $("#email").val();
+	var bDay= $("#bDay).val();
+	var bMonth= $("#bMonth").val();
+	var bYear= $("#bYear").val();
+	var homePhone= $("#home").val();
+	var mobilePhone= $("#mobile").val();
+	var drivingLicense= $("#license").val();
+	var bDate = new Date(bYear, bMonth, bDay);
+	var newUser = new User (userName, password, email, bDate, homePhone, mobilePhone, drivingLicense);
+}
 
 });
-
