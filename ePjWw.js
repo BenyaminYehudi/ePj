50de3270103055c40f9b5ab14723ac23ae93bbc9
BenyaminYehudi <benyamin.yehudi@gmail.com>
2015-01-04 18:25:28 +0200

    Update ePjWw.js

--- ePjWw.js
+++ ePjWw.js
@@ -1,3 +1,36 @@
+var distances = {
+  "Jerusalem":{
+    "Jerusalem":0,
+    "Tel Aviv":67.4,
+    "Eilat":315,
+    "Tiberias":178,
+    "Haifa":151},
+  "Tel Aviv":{
+    "Tel Aviv":0,
+    "Jerusalem":67.4,
+    "Eilat":346,
+    "Tiberias":136,
+    "Haifa":91.8},
+  "Eilat":{
+    "Eilat":0,
+    "Jerusalem":315,
+    "Tel Aviv":346,
+    "Tiberias":455,
+    "Haifa":428},
+  "Haifa":{
+    "Haifa":0,
+    "Jerusalem":151,
+    "Tel Aviv":91.8,
+    "Eilat":428,
+    "Tiberias":63.3},
+  "Tiberias":{
+    "Tiberias":0,
+    "Jerusalem":178,
+    "Tel Aviv":136,
+    "Eilat":455,
+    "Haifa":428}
+};
+
 function calculate(){
   var source = document.getElementById("source").value;
   var destination = document.getElementById("destination").value;
