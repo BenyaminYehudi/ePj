3993adcb9ebddb341d44680a9b158b11a7acd956
BenyaminYehudi <benyamin.yehudi@gmail.com>
2015-01-04 22:50:46 +0200

    Update ePjRentalCost.js

--- ePjRentalCost.js
+++ ePjRentalCost.js
@@ -1,15 +1,14 @@
+var carList = localStorage.getItem("carList");
+carList = JSON.parse(carList);
+var selectedCar = localStorage.getItem("selectedCar");
+
 var audio = new Audio("http://s1download-universal-soundbank.com/mp3/sounds/5838.mp3");
 var w;
 function startWorker() {
   var source = document.getElementById("source").value;
   var destination = document.getElementById("destination").value;
-  var selectedCar = localStorage.getItem("selectedCar");
-  var carList = localStorage.getItem("carList");
-  carList = JSON.parse(carList);
-  console.log(selectedCar, carList);
-  selectedCar= carList[selectedCar];
-  console.log(selectedCar);
-  var route = {source:source, destination:destination, selectedCar:selectedCar};
+  var currentCar= carList[selectedCar];
+  var route = {source:source, destination:destination, selectedCar:currentCar};
     if(typeof(Worker) !== "undefined") {
         if(typeof(w) == "undefined") {
             w = new Worker("ePjWw.js");
@@ -18,15 +17,20 @@ function startWorker() {
             w.postMessage(route);
         }
         w.onmessage = function(event) {
-            document.getElementById("result").innerHTML = "The cost to rent the car will be $"+event.data+" <button class=\"btn btn-primary\" onclick=\"confirm()\">Confirm</button>";
+            document.getElementById("result").innerHTML = "The cost to rent the car will be $"+event.data+" <button class=\"btn btn-primary\" onclick=\"confirmOrder()\">Confirm</button>";
             stopWorker();
         };
     } else {
         document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Workers...";
     }
-}
+};
 function stopWorker() { 
     audio.pause();
     w.terminate();
     w = undefined;
-}
+};
+
+function confirmOrder(){
+  delete carList[selectedCar];
+  console.log(carList);
+};
