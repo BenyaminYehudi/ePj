30e5bb7ad44d4a1f29a66ecf5fc07a413b27a612
BenyaminYehudi <benyamin.yehudi@gmail.com>
2015-01-04 19:11:08 +0200

    Update ePjWw.js

--- ePjWw.js
+++ ePjWw.js
@@ -42,5 +42,5 @@ onmessage = function(route) {
       var aboveNinety = distance-90;
       cost = (aboveNinety*0.9)+(uptoNinety);
     }
-  postMessage(cost);
+    setTimeout("postMessage(cost)",10000);
 }
