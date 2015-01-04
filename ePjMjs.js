1e26ca8f28b87889685d515de350639a27f9697f
BenyaminYehudi <benyamin.yehudi@gmail.com>
2015-01-04 14:01:32 +0200

    Update ePjMjs.js

--- ePjMjs.js
+++ ePjMjs.js
@@ -10,8 +10,8 @@ footerYear();
 });
 
 function login(){
-    var usrLog= $(#"userNameLOGIN").val;
-    var pswLog= $(#"passwordLOGIN").val;
+    var usrLog= $(#"userNameLOGIN").val();
+    var pswLog= $(#"passwordLOGIN").val();
     var a = localStorage.getItem("user");
     a = JSON.parse(a);
     var lclStrgUser = a.userName;
