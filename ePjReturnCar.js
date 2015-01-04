b6bc88c70820f634ec7adc21307a50e8628186ae
BenyaminYehudi <benyamin.yehudi@gmail.com>
2015-01-04 23:56:22 +0200

    Update ePjReturnCar.js

--- ePjReturnCar.js
+++ ePjReturnCar.js
@@ -2,7 +2,7 @@ function carReturn(){
   var licensePlate = $("#returnCarLicense").val();
   var rentedCars = JSON.parse(localStorage.getItem("rentedCars"));
   console.log(rentedCars);
-  if (rentedCars[licensePlate] !== null){
+  if (rentedCars[licensePlate] !== undefined){
     console.log("it exists");
   } else {
     console.log("it doesn't exist");
