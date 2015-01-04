6111686c2eb55fb055ac1b639af01ec8e955e133
BenyaminYehudi <benyamin.yehudi@gmail.com>
2015-01-04 17:33:30 +0200

    Update ePjRentalCost.js

--- ePjRentalCost.js
+++ ePjRentalCost.js
@@ -1,3 +1,31 @@
+var distances = {
+  "Jerusalem":{
+    "Tel Aviv":67.4,
+    "Eilat":315,
+    "Tiberias":178,
+    "Haifa":151},
+  "Tel Aviv":{
+    "Jerusalem":67.4,
+    "Eilat":346,
+    "Tiberias":136,
+    "Haifa":91.8},
+  "Eilat":{
+    "Jerusalem":315,
+    "Tel Aviv":346,
+    "Tiberias":455,
+    "Haifa":428},
+  "Haifa":{
+    "Jerusalem":151,
+    "Tel Aviv":91.8,
+    "Eilat":428,
+    "Tiberias":63.3},
+  "Tiberias":{
+    "Jerusalem":178,
+    "Tel Aviv":136,
+    "Eilat":455,
+    "Haifa":428}
+};
+
 function(){
 
 }
