756db0b4976ea11321af4f16cbb0bd5e5ae2acfc
BenyaminYehudi <benyamin.yehudi@gmail.com>
2015-01-04 13:55:19 +0200

    Update ePjMjs.js

--- ePjMjs.js
+++ ePjMjs.js
@@ -5,7 +5,16 @@ function footerYear() {
     var n = d.getFullYear();
     document.getElementById("footerYear").innerHTML = n;
 }
-
 footerYear();
 
 });
+
+function login(){
+    var usrLog= $(#"userNameLOGIN").val;
+    var pswLog= $(#"passwordLOGIN").val;
+    var a = localStorage.getItem("user");
+    a = JSON.parse(a);
+    var lclStrgUser = a.userName;
+    var lclStrgPass = a.password;
+    if()
+}
