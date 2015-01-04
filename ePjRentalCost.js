e656e5e940670d7847ab6cb0bd43521bbc90f0fa
BenyaminYehudi <benyamin.yehudi@gmail.com>
2015-01-04 18:29:15 +0200

    Update ePjRentalCost.js

--- ePjRentalCost.js
+++ ePjRentalCost.js
@@ -1,41 +1,12 @@
-var distances = {
-  "Jerusalem":{
-    "Jerusalem":0,
-    "Tel Aviv":67.4,
-    "Eilat":315,
-    "Tiberias":178,
-    "Haifa":151},
-  "Tel Aviv":{
-    "Tel Aviv":0,
-    "Jerusalem":67.4,
-    "Eilat":346,
-    "Tiberias":136,
-    "Haifa":91.8},
-  "Eilat":{
-    "Eilat":0,
-    "Jerusalem":315,
-    "Tel Aviv":346,
-    "Tiberias":455,
-    "Haifa":428},
-  "Haifa":{
-    "Haifa":0,
-    "Jerusalem":151,
-    "Tel Aviv":91.8,
-    "Eilat":428,
-    "Tiberias":63.3},
-  "Tiberias":{
-    "Tiberias":0,
-    "Jerusalem":178,
-    "Tel Aviv":136,
-    "Eilat":455,
-    "Haifa":428}
-};
-
 var w;
 function startWorker() {
+  var source = document.getElementById("source").value;
+  var destination = document.getElementById("destination").value;
+  var route = {source:source, destination:destination};
     if(typeof(Worker) !== "undefined") {
         if(typeof(w) == "undefined") {
             w = new Worker("ePjWw.js");
+            w.postMessage(route);
         }
         w.onmessage = function(event) {
             document.getElementById("result").innerHTML = event.data;
