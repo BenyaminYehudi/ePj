3b8e49e76ee73d42b0f35f4294221173024a94cb
BenyaminYehudi <benyamin.yehudi@gmail.com>
2015-01-04 14:32:51 +0200

    Update ePjMjs.js

--- ePjMjs.js
+++ ePjMjs.js
@@ -16,7 +16,9 @@ function login(){
     a = JSON.parse(a);
     var lclStrgUser = a.userName;
     var lclStrgPass = a.password;
-    console.log(lclStrgUser);
-    console.log(lclStrgPass);
-    /*if()*/
+    if(usrLog===lclStrgUser && pswLog===lclStrgPass){
+        localStorage.setItem("loggedIn", true);
+    } else {
+        localStorage.setItem("loggedIn", false);
+    }
 }
