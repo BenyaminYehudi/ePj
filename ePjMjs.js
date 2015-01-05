63118dba55e3cf9f3d20c0aed6a1c67828b3320c
BenyaminYehudi <benyamin.yehudi@gmail.com>
2015-01-05 10:31:37 +0200

    Update ePjMjs.js

--- ePjMjs.js
+++ ePjMjs.js
@@ -26,11 +26,28 @@ function login(){
     }
 };
 
+function loginPopup(){
+    var usrLog= $("#popupUser").val();
+    console.log(usrLog);
+    var pswLog= $("#popupPass").val();
+    console.log(pswLog);
+    var a = localStorage.getItem("user");
+    a = JSON.parse(a);
+    var lclStrgUser = a.userName;
+    var lclStrgPass = a.password;
+    if(usrLog===lclStrgUser && pswLog===lclStrgPass){
+        localStorage.setItem("loggedIn", true);
+        checkIfLogged();
+    } else {
+        localStorage.setItem("loggedIn", false);
+    }
+};
+
 function checkIfLogged(){
     if(localStorage.getItem("loggedIn")==="true"){
         $("#loginDiv").html('<button class="btn btn-primary" onclick="logOut()">Log Out</button>');        
     } else {
-        $("#light").html('Please register if you\'re not already a JB Registered Renter, or login (using the form above), in order to continue <a href="ePjRegister.html"><button class="btn btn-primary">Register</button></a>').css("display","block");
+        $("#light").html('<div id="popupLogin"><ul><li><input type="text" name="username" id="popupUser" placeholder="Username"></li><li><input type="password" name="password" id="popupPass" placeholder="Password"></li><li><button class="btn btn-primary" onclick="loginPopup()">LOGIN</button></li></ul></div>').css("display","block");
         $("#fade").css("display","block");
     }
 };
