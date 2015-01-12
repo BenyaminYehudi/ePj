3f0481ab11a3f32a10b8f1019aca8b20de14ec3a
BenyaminYehudi <benyamin.yehudi@gmail.com>
2015-01-12 15:57:25 +0200

    Update ePjMjs.js

--- ePjMjs.js
+++ ePjMjs.js
@@ -45,11 +45,14 @@ function loginPopup(){
 
 function checkIfLogged(){
     if(localStorage.getItem("loggedIn")==="true"){
-        $("#loginDiv").html('<button class="btn btn-primary" onclick="logOut()">Log Out</button>');
+        $("#loginDiv").html('<span id="loggedUser"></span><button class="btn btn-primary" onclick="logOut()">Log Out</button>');
         $("#sideNav").html('<h4>Our Fleet</h4><ul><li><a href="ePjCars.html"><button class="btn btn-primary">Our Cars</button></a></li></ul><h4>Returning a car?<h4><ul><li><a href="ePjReturnCar.html"><button class="btn btn-primary">Return Car</button></a></li></ul>');
         $("#light").css("display","none");
         $("#fade").css("display","none");
         $("body").css("overflow","visible");
+        var a = localStorage.getItem("user");
+        a = JSON.parse(a);
+        $("#loggedUser").val(a.userName);
     } else {
         $("#light").html('<div id="popupLogin"><ul><li><input type="text" name="username" id="popupUser" placeholder="Username"></li><li><input type="password" name="password" id="popupPass" placeholder="Password"></li><li><button class="btn btn-primary" onclick="loginPopup()">LOGIN</button></li></ul></div><div id="popupRegister"><h1>Not a Registered Renter?</h1><h3>Sign up now!</h3><a href="ePjRegister.html"><button class="btn btn-primary">Register</button></a></div>').css("display","block");
         $("#fade").css("display","block");
