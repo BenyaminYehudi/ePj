01af9cb7b15d7ed585932c25d559398f7692bfa2
BenyaminYehudi <benyamin.yehudi@gmail.com>
2015-01-12 15:58:37 +0200

    Update ePjMjs.js

--- ePjMjs.js
+++ ePjMjs.js
@@ -52,7 +52,7 @@ function checkIfLogged(){
         $("body").css("overflow","visible");
         var a = localStorage.getItem("user");
         a = JSON.parse(a);
-        $("#loggedUser").val(a.userName);
+        $("#loggedUser").html(a.userName);
     } else {
         $("#light").html('<div id="popupLogin"><ul><li><input type="text" name="username" id="popupUser" placeholder="Username"></li><li><input type="password" name="password" id="popupPass" placeholder="Password"></li><li><button class="btn btn-primary" onclick="loginPopup()">LOGIN</button></li></ul></div><div id="popupRegister"><h1>Not a Registered Renter?</h1><h3>Sign up now!</h3><a href="ePjRegister.html"><button class="btn btn-primary">Register</button></a></div>').css("display","block");
         $("#fade").css("display","block");
