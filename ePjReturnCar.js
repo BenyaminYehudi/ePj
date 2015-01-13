709f8e1fd51a9010318e193f6e7e1d5cd0450817
BenyaminYehudi <benyamin.yehudi@gmail.com>
2015-01-14 00:55:33 +0200

    Update ePjReturnCar.js

--- ePjReturnCar.js
+++ ePjReturnCar.js
@@ -8,8 +8,16 @@ function carReturn(){
     localStorage.setItem("carList", JSON.stringify(carList));
     delete rentedCars[licensePlate];
     localStorage.setItem("rentedCars", JSON.stringify(rentedCars));
-    document.getElementById("result").innerHTML = "The car has been returned successfully!";
+    $("#returnedCar").html('<h3>The car has been returned successfully!</h3><a href="index.html"><button class="btn btn-primary">Main Page</button></a>').css("display","block");
+    $("#fade").css("display","block");
   } else {
     document.getElementById("result").innerHTML = "There is no such vehicle in our fleet. Please check the license number and try again";
+    $("#returnedCar").html('<h3>There is no such vehicle in our fleet. Please check the license number and try again.</h3><button class="btn btn-primary" onclick="closePopup()">Close</button>').css("display","block");
+    $("#fade").css("display","block");
   }
 };
+
+function closePopup(){
+  $("#returnedCar").css("display","none");
+  $("#fade").css("display","none");
+}
