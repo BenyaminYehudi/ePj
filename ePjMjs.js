cbecafc24cd92ea6bec0acba8fa27fcc7d82cb00
BenyaminYehudi <benyamin.yehudi@gmail.com>
2015-01-14 20:32:05 +0200

    Update ePjMjs.js

--- ePjMjs.js
+++ ePjMjs.js
@@ -21,8 +21,8 @@ function login(){
         checkIfLogged();
     } else {
         localStorage.setItem("loggedIn", false);
-        alert("Wrong username or password");
-       /* $('<div class="loginError">Wrong username or password!t</div>').appendTo('.logRegContainer'); */
+        $("#light").html('<div class="loginError">Wrong username or password!</div><button class="btn btn-primary" onclick="closePopup()">Close</button>').css("display","block");
+        $("#fade").css("display","block");
     }
 };
 
@@ -38,7 +38,7 @@ function loginPopup(){
         checkIfLogged();
     } else {
         localStorage.setItem("loggedIn", false);
-        $('<div class="loginError">Wrong username or password!t</div>').appendTo('.logRegContainer');
+        $('<div class="loginErrorPopup">Wrong username or password!</div>').appendTo('.logRegContainer');
     }
 };
 
@@ -48,14 +48,12 @@ function checkIfLogged(){
         $("#sideNav").html('<h4>Home Page</h4><ul><li><a href="index.html"><button class="btn btn-primary">Home</button></a></li></ul><h4>Our Fleet</h4><ul><li><a href="ePjCars.html"><button class="btn btn-primary">Our Cars</button></a></li></ul><h4>Returning a car?<h4><ul><li><a href="ePjReturnCar.html"><button class="btn btn-primary">Return Car</button></a></li></ul>');
         $("#light").css("display","none");
         $("#fade").css("display","none");
-        $("body").css("overflow","visible");
         var a = localStorage.getItem("user");
         a = JSON.parse(a);
         $("#loggedUser").html(a.userName);
     } else {
         $("#light").html('<div class="logRegContainer"><div id="popupLogin"><h1>Login if you\'re already a member:</h1><ul><li><input type="text" name="username" id="popupUser" placeholder="Username"></li><li><input type="password" name="password" id="popupPass" placeholder="Password"></li><li><button class="btn btn-primary" onclick="loginPopup()">LOGIN</button></li></ul></div><div id="popupRegister"><h1>Not a Registered Renter?</h1><h3>Sign up now!</h3><a href="ePjRegister.html"><button class="btn btn-primary">Register</button></a></div></div>').css("display","block");
         $("#fade").css("display","block");
-        $("body").css("overflow","hidden");
     }
 };
 
@@ -63,3 +61,7 @@ function logOut(){
     localStorage.setItem("loggedIn", false);
     location.reload(true);
 };
+
+function closePopup(){
+	window.location="index.html"
+}
