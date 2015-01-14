8346e807bfe33479ef2e290ae535e852c8cef483
BenyaminYehudi <benyamin.yehudi@gmail.com>
2015-01-14 02:20:26 +0200

    Update ePjRegister.js

--- ePjRegister.js
+++ ePjRegister.js
@@ -40,7 +40,7 @@ function register(){
 	localStorage.setItem("user", JSON.stringify(newUser));
 	$("#logInPopup").html('<div id="popupLoginRegst"><h1>Login:</h1><ul><li><input type="text" name="username" id="popupUserRegst" placeholder="Username"></li><li><input type="password" name="password" id="popupPassRegst" placeholder="Password"></li><li><button class="btn btn-primary" onclick="loginPopupRegst()">LOGIN</button></li></ul></div>').css("display","block");
         $("#logInPopupFade").css("display","block");
-        checkIfRegistered();
+       /* checkIfRegistered(); */
 }
 
 function refreshed(){
